61c6d770dd20bf29ad4e84a5278ba1140651aa15
dijeungi <dijeungi@naver.com>
2025-11-23 14:33:28 +0900

    refactor: 환경변수 의존으로 수정

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -22,7 +22,7 @@ export class JwtStrategy extends PassportStrategy(JwtStrategyBase) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET') ?? 'change-me',
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
