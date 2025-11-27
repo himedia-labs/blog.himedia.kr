7c965d4d1cd58732355c773e0c8a770fef15ae15
dijeungi <dijeungi@naver.com>
2025-11-28 08:57:29 +0900

    refactor: jwt 최적화

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,40 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
+
+import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 import { TokenService } from './services/token.service';
-import { PasswordResetService } from './services/password-reset.service';
+import { PasswordService } from './services/password.service';
+
 import { User } from './entities/user.entity';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { RefreshToken } from './entities/refreshToken.entity';
 import { PasswordReset } from './entities/passwordReset.entity';
+
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { getRequiredEnv } from '../common/exception/config.exception';
+
 import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, RefreshToken, PasswordReset]),
     PassportModule,
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const secret = getRequiredEnv(configService, 'JWT_SECRET');
-        const expiresIn = getRequiredEnv(
-          configService,
-          'ACCESS_TOKEN_EXPIRES_IN',
-        ) as JwtSignOptions['expiresIn'];
-
-        return {
-          secret,
-          signOptions: {
-            expiresIn,
-          },
-        };
+    JwtModule.register({
+      secret: process.env.JWT_SECRET!,
+      signOptions: {
+        expiresIn: Number(process.env.ACCESS_TOKEN_EXPIRES_IN),
       },
     }),
     EmailModule,
@@ -43,7 +35,8 @@ import { EmailModule } from '../email/email.module';
   providers: [
     AuthService,
     TokenService,
-    PasswordResetService,
+    PasswordService,
+    UserService,
     LocalStrategy,
     JwtStrategy,
   ],
