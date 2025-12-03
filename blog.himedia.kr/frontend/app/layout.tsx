bc7a8cbbf3b372ee69727f0bd2e0da94d4531c3c
dijeungi <dijeungi@naver.com>
2025-12-03 14:58:25 +0900

    chore: Header, Footer, ToastProvider 를 layout으로 분리

--- blog.himedia.kr/frontend/app/layout.tsx
+++ blog.himedia.kr/frontend/app/layout.tsx
@@ -1,41 +1,19 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-
+import LayoutContent from './shared/layout/LayoutContent';
 import QueryProvider from './provider/ReactQuery/QueryProvider';
 
 import './globals.css';
 import './reset.css';
-import Header from './shared/components/header/Header';
-import Footer from './shared/components/footer/Footer';
-import ScrollTopButton from './shared/components/scroll-top/ScrollTopButton';
-import ToastProvider from './shared/components/toast/ToastProvider';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
-  // Header를 숨길 경로들
-  const hideHeaderPaths = [''];
-  const hideHeader = hideHeaderPaths.includes(pathname);
-
-  // Footer를 숨길 경로들
-  const hideFooterPaths = [''];
-  const hideFooter = hideFooterPaths.includes(pathname);
-
   return (
     <html lang="ko">
       <body>
         <QueryProvider>
-          <ToastProvider>
-            {!hideHeader && <Header />}
-            <div className="layout">{children}</div>
-            {!hideFooter && <Footer />}
-            {!hideFooter && <ScrollTopButton />}
-          </ToastProvider>
+          <LayoutContent>{children}</LayoutContent>
         </QueryProvider>
       </body>
     </html>
