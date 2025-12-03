b0e71090b3a522b2c4a1f96bee0b16beea62370e
dijeungi <dijeungi@naver.com>
2025-12-03 14:58:37 +0900

    chore: 경로 수정

--- blog.himedia.kr/frontend/app/page.tsx
+++ blog.himedia.kr/frontend/app/page.tsx
@@ -1,5 +1,5 @@
-import Banner from './(routes)/(public)/main/components/Banner';
-import BlogListSection from './(routes)/(public)/main/components/BlogList';
+import Banner from './(routes)/(public)/main/components/banner/banner';
+import BlogListSection from './(routes)/(public)/main/components/blogList/blogList';
 
 export default function Home() {
   return (
