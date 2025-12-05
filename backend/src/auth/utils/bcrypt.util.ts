17e25d87e10310fe0b20ba8f54253e7f05211bd3
dijeungi <dijeungi@naver.com>
2025-12-05 17:11:37 +0900

    chore: .prettier으로 인한 라인 수정

--- backend/src/auth/utils/bcrypt.util.ts
+++ backend/src/auth/utils/bcrypt.util.ts
@@ -1,1 +1,7 @@
-export { hash as hashPassword, compare as comparePassword } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
+
+import { AUTH_CONFIG } from '../../constants/config/auth.config';
+
+export const hashPassword = hash;
+export const comparePassword = compare;
+export const hashWithAuthRounds = (value: string): Promise<string> => hash(value, AUTH_CONFIG.BCRYPT_ROUNDS);
