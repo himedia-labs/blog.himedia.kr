9a19b1c65321a513edf4b5008c51d60462c0bae3
dijeungi <dijeungi@naver.com>
2026-01-18 17:34:43 +0900

    chore: import 순서 정리

--- frontend/app/(routes)/(private)/posts/drafts/page.tsx
+++ frontend/app/(routes)/(private)/posts/drafts/page.tsx
@@ -2,8 +2,8 @@
 
 import Link from 'next/link';
 
-import { useDraftsQuery } from '@/app/api/posts/posts.queries';
 import { useAuthStore } from '@/app/shared/store/authStore';
+import { useDraftsQuery } from '@/app/api/posts/posts.queries';
 
 import styles from './DraftList.module.css';
 
