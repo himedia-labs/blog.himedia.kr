6db676b721e5b8920a145d9ed2b209553717a206
dijeungi <dijeungi@naver.com>
2026-02-01 19:16:26 +0900

    refactor: QueryProvide를 공식 패턴으로 단순화

--- frontend/app/provider/ClientProvider/ClientProvider.types.ts
+++
@@ -1,3 +0,0 @@
-export interface ClientProviderProps {
-  children: React.ReactNode;
-}
--- frontend/app/provider/ReactQuery/QueryProvider.tsx
+++ frontend/app/provider/ReactQuery/QueryProvider.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { getQueryClient } from './getQueryClient';
-
-export default function QueryProvider({ children }: PropsWithChildren) {
-  const queryClient = getQueryClient();
+/**
+ * React Query 프로바이더
+ * @description QueryClient를 생성하고 앱 전역에 주입
+ */
+export default function QueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
